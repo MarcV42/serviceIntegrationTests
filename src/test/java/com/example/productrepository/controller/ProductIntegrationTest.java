9d5dad22b6b1c321bb30db5d250e38ec2bddcffb
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-09-18 17:13:23 +0200

    TestGetAllProductsw

--- src/test/java/com/example/productrepository/controller/ProductIntegrationTest.java
+++ src/test/java/com/example/productrepository/controller/ProductIntegrationTest.java
@@ -1,15 +1,18 @@
-package com.example.productrepository.products;
+package com.example.productrepository.controller;
 
+import com.example.productrepository.products.ProductRepository;
 import com.example.productrepository.products.models.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,22 +27,29 @@ public class ProductIntegrationTest {
 
 
     @Test
+    @DirtiesContext
     void testGetAllProducts() throws Exception {
         //Given
         productRepository.save(new Product("1", "test", 5));
 
 
-// hier kann man statt get auch post eingeben um eine postmethode zu testen
-//When
+        // hier kann man statt get auch post eingeben um eine postmethode zu testen
+        //When
         mockMvc.perform(MockMvcRequestBuilders.get("/api/products"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
-[
-]
-"""
-));
+                        [
+                        {
+                        "id": "1",
+                        "title": "test",
+                        "price": 5
+                        }
+                        ]
+                        """
+                ));
 
 
         //THen
     }
 }
+
